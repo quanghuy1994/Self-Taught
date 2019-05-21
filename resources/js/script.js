bfa8a59b5092e6ac9177f3424288814473b5ddb8
quanghuy1994 <votrongquanghuy@gmail.com>
2019-05-21 13:57:05 +0800

    commit

---
+++ resources/js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(
+    function(){
+        //sticky nav
+        $('.about-section').waypoint(
+            function(direction){
+                if(direction == 'down'){
+                    $('nav').addClass('sticky');
+                }
+                else{
+                    $('nav').removeClass('sticky');
+                }
+            },{
+                offset: '100px'
+            }
+        )
+        //scroll
+        $('a').click(function(event){
+            $('html, body').animate({
+                scrollTop: $( $.attr(this, 'href') ).offset().top
+            }, 700);
+            event.preventDefault();
+        });
+        //mobile nav
+        $('.mobile-icon').click(
+            function(){
+                $('.main-nav').slideToggle(200);
+                
+                if($('.mobile-icon').hasClass('fa-bars')){
+                    $('.mobile-icon').addClass('fa-times')
+                    $('.mobile-icon').removeClass('fa-bars')
+                }
+                else{
+                    $('.mobile-icon').addClass('fa-bars')
+                    $('.mobile-icon').removeClass('fa-times')
+                }
+            }
+           
+        )
+    }
+)
+    
